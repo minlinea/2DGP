e393982ea8e39c391fd01cea66b2a89a030ad9be
JungMinSu(MSI) <min_linea@naver.com>
2018-09-18 19:05:48 +0900

    movement_calculation() 함수 작성
    momentum_control이라는 변수를 통해서 캐릭터의 운동량을 조절하는 값을 부여하여 차후의 좀 더 빠르게, 느리게 할 수 도 있기 때문에 쉽게 조절토록 하였음.
    또한 기울기의 변화량의 연산을 통해 캐릭터의 움직임을 계산했음
    straight_move() 경우 연산을 위한 x,y 값에서 하나 더 추가됐으나 모호한 변수 탓에 구체적으로 이름을 명시함

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -5,14 +5,18 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+
 def straight_move():
-    x, y = 203, 535
-    x, y = movement_calculation(x,y)
+    character_x, character_y = 203, 535
+    destination_x, destination_y = 132, 243
+    character_x, character_y = movement_calculation(character_x, character_y, destination_x, destination_y)
     pass
 
-def movement_calculation(x,y):
-    return x, y
-    pass
+def movement_calculation(x1, y1, x2, y2):
+    momentum_control = 16
+    x1 += (x2-x1) / momentum_control
+    y1 += ((y2-y1) / (x2-x1)) * ((x2-x1) / momentum_control)
+    return x1, y1
 
 def move_to_point():
     straight_move()
