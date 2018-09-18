b4a5ba400d803dc67f54c33b9cfef7c1b9def314
JungMinSu(MSI) <min_linea@naver.com>
2018-09-18 21:00:18 +0900

    straight_move() 함수 수정 2차
    각 포인트들을 튜플로 묶어 숫자로 일일이 고치는 것이 아닌 해당 튜플만 부르면 가능하게 끔 작성
    또한 frame처럼 now_index의 변환도 %이용 작성
    단순 숫자로 적혀있어 가독성이 떨어지는 숫자들 영어 인자를 통한 가독성 향상 추가

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -5,23 +5,45 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
+point0 = (203, 535)
+point1 = (132, 243)
+point2 = (535, 470)
+point3 = (477, 203)
+point4 = (715, 136)
+point5 = (316, 225)
+point6 = (510, 92)
+point7 = (692, 518)
+point8 = (682, 336)
+point9 = (712, 349)
+point_dictionary = (point0, point1, point2, point3, point4, point5, point6, point7, point8, point9)
+
+image_size = 100
+image_point = 100
+
 grass_pibotx, grass_piboty = 400, 30
 
+now_index = 0
+next_index = now_index+1
+frame = 0
+
 def straight_move():
-    frame = 0
-    character_x, character_y = 203, 535
-    destination_x, destination_y = 132, 243
-    while (character_x > 132 + 1):
+    global frame
+    global now_index
+    global next_index
+    if(now_index == 9):
+        next_index = 0
+    character_x, character_y = point_dictionary[now_index]
+    destination_x, destination_y =  point_dictionary[next_index]
+    while (character_x < point_dictionary[now_index][0]+ 1):
         clear_canvas()
-        character.clip_draw(frame * 100, 0 * 100, 100, 100, character_x, character_y)
+        character.clip_draw(frame * image_point, 0 * image_point, image_size, image_size, character_x, character_y)
         grass.draw(grass_pibotx, grass_piboty)
         update_canvas()
         frame = (frame + 1) % 8
         character_x, character_y = movement_calculation(character_x, character_y, destination_x, destination_y)
         delay(0.05)
         get_events()
-
-
+    now_index = (now_index + 1) % 10
     pass
 
 def movement_calculation(x1, y1, x2, y2):
