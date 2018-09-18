7af8c7fe8f69c2565a6eccd5f5b56bb328ee9ca6
JungMinSu(MSI) <min_linea@naver.com>
2018-09-18 20:17:08 +0900

    straight_move() 함수 1차 작성
    1번째 점과 2번째 점을 기준으로만 정상 이동하며, 애니메이션 및 이미지를 버퍼를 이용하여 그렸음.
    원래 movement_calculation()이 의도했던 바대로 움직인 것은 아니었으나 생각보다 마음에 들어서 이대로 가기로 하였으며, 다만 극한처럼 그 값에 도달하지는 못하기 때문에 straight_move()
     조건문에 character_x > 132 + 1을 추가함

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -3,17 +3,29 @@ from pico2d import *
 open_canvas()
 
 grass = load_image('grass.png')
-character = load_image('character.png')
+character = load_image('animation_sheet.png')
 
+grass_pibotx, grass_piboty = 400, 30
 
 def straight_move():
+    frame = 0
     character_x, character_y = 203, 535
     destination_x, destination_y = 132, 243
-    character_x, character_y = movement_calculation(character_x, character_y, destination_x, destination_y)
+    while (character_x > 132 + 1):
+        clear_canvas()
+        character.clip_draw(frame * 100, 0 * 100, 100, 100, character_x, character_y)
+        grass.draw(grass_pibotx, grass_piboty)
+        update_canvas()
+        frame = (frame + 1) % 8
+        character_x, character_y = movement_calculation(character_x, character_y, destination_x, destination_y)
+        delay(0.05)
+        get_events()
+
+
     pass
 
 def movement_calculation(x1, y1, x2, y2):
-    momentum_control = 16
+    momentum_control = 10
     x1 += (x2-x1) / momentum_control
     y1 += ((y2-y1) / (x2-x1)) * ((x2-x1) / momentum_control)
     return x1, y1
