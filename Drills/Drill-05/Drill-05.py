0d819087ddc98eb24cb718822d3540e3a1dac3b4
JungMinSu(MSI) <min_linea@naver.com>
2018-09-18 18:42:30 +0900

    이동 알고리즘 완성
    straight_move() 함수를 추가하여 알고리즘은 잡았으나 아직 변동의 가능성의 있어 move_to_point()의 pass를 제거하지 않았음

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -5,7 +5,12 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+def straight_move():
+    pass
+
+
 def move_to_point():
+    straight_move()
     pass
 
 while True:
