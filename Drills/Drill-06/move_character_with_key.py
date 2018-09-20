751179faa2062a3cac5ff10a08f70c480a8fa6ed
JungMinSu(MSI) <min_linea@naver.com>
2018-09-20 18:14:49 +0900

    애니메이션 '멈춤' 추가
    키보드에서 손을 뗐을 때 멈추는 것 추가

--- Drills/Drill-06/move_character_with_key.py
+++ Drills/Drill-06/move_character_with_key.py
@@ -2,7 +2,7 @@ from pico2d import *
 
 
 def handle_events():
-    global running, dir, right, left, point
+    global running, dir, right, left, Facing_point, move
 
     events = get_events()
     for event in events:
@@ -11,27 +11,32 @@ def handle_events():
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
                 dir += 1
-                point = right
+                move = True
+                Facing_point = right
             elif event.key == SDLK_LEFT:
                 dir -= 1
-                point = left
+                move = True
+                Facing_point = left
             elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
                 dir -= 1
+                move = False
             elif event.key == SDLK_LEFT:
                 dir +=1
+                move = False
 
 left = 0
 right =1
 Facing_Direction = (left, right)
-point = 1
+Facing_point = 1
 
 open_canvas()
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
+move = False
 running = True
 x = 800 // 2
 frame = 0
@@ -40,10 +45,13 @@ dir = 0
 while (x<800 and running):
     clear_canvas()
     grass.draw(400, 30)
-    character.clip_draw(frame * 100, 100 * Facing_Direction[point], 100, 100, x, 90)
+    character.clip_draw(frame * 100, 100 * Facing_Direction[Facing_point], 100, 100, x, 90)
     update_canvas()
     handle_events()
-    frame = (frame + 1) % 8
+    if move == True:
+        frame = (frame + 1) % 8
+    else:
+        frame = 0
     x += dir * 5
     delay(0.05)
 
