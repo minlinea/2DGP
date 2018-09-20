e403a0b941b8125400470d8de2f0edc7eee353a5
JungMinSu(MSI) <min_linea@naver.com>
2018-09-20 18:46:51 +0900

    변수 정리 및 클릭 구현
    마우스, 마우스클릭, 캐릭터의 인자 가독성을 위한 명칭 정리 및 마우스 클릭에 따른 해당 위치 캐릭터 생성 완성
    ypos 조정을 위한 함수 생성

--- Drills/Drill-06/Drill-06.py
+++ Drills/Drill-06/Drill-06.py
@@ -5,20 +5,26 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 def handle_events():
     global running
-    global xpos,ypos
+    global mouse_xpos, mouse_ypos, character_xpos, character_ypos, mouseclick_ypos, mouseclick_xpos
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSEMOTION:
-            xpos,ypos = event.x, KPU_HEIGHT -1 - event.y
+            mouse_xpos, mouse_ypos = event.x, window_to_pico_coordinate_system(event.y)
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            mouseclick_xpos, mouseclick_ypos = event.x,  window_to_pico_coordinate_system(event.y)
+            character_xpos, character_ypos = mouseclick_xpos, mouseclick_ypos
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
 def straight_move():
     pass
 
-def moving_Direction():
+def window_to_pico_coordinate_system(num):
+    return KPU_HEIGHT - 1 - num
+
+def moving_direction(character_X, now_indexX, next_indexX):
     pass
 
 def movement_calculation(x1, y1, x2, y2):
@@ -30,16 +36,31 @@ kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 hand_arrow = load_image('hand_arrow.png')
 
+
+
 running = True
-xpos, ypos = KPU_WIDTH // 2, KPU_HEIGHT // 2
+click = True
+
+mouse_xpos, mouse_ypos = KPU_WIDTH // 2, KPU_HEIGHT // 2
+character_xpos, character_ypos = KPU_WIDTH // 2, KPU_HEIGHT // 2
+mouseclick_xpos, mouseclick_ypos = KPU_WIDTH // 2, KPU_HEIGHT // 2
+
+
 frame = 0
 hide_cursor()
+
+run_left = 0
+run_right = 1
+walk_left = 2
+walk_right = 3
+facing_direction = (run_left, run_right, walk_left, walk_right)
+facing_point = 0
 
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, xpos, ypos)
-    hand_arrow.draw(xpos, ypos)
+    character.clip_draw(frame * 100, 100 * facing_direction[facing_point], 100, 100, character_xpos, character_ypos)
+    hand_arrow.draw(mouse_xpos, mouse_ypos)
     straight_move()
     update_canvas()
     frame = (frame + 1) % 8
