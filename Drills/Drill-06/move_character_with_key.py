f749d4428523e1c1919de769a4f5414cd58c0993
JungMinSu(MSI) <min_linea@naver.com>
2018-09-20 18:06:14 +0900

    키보드 입력을 통한 캐릭터 움직임 구현
    left, right 키입력을 받고 이동하는 것은 구현했으나, 불만사항이 너무 많은걸?

--- Drills/Drill-06/move_character_with_key.py
+++ Drills/Drill-06/move_character_with_key.py
@@ -2,8 +2,20 @@ from pico2d import *
 
 
 def handle_events():
-    # fill here
-    pass
+    global running
+    global x
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                x += 10
+            elif event.key == SDLK_LEFT:
+                x -= 10
+            elif event.key == SDLK_ESCAPE:
+                running = False
 
 
 open_canvas()
@@ -14,7 +26,7 @@ running = True
 x = 800 // 2
 frame = 0
 
-while running:
+while (x<800 and running):
     clear_canvas()
     grass.draw(400, 30)
     character.clip_draw(frame * 100, 100 * 1, 100, 100, x, 90)
