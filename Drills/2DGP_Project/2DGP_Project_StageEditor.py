d83a30d13bd46f0655c543f66aaa8dfb354e944e
JungMinSu(MSI) <min_linea@naver.com>
2018-10-01 21:42:17 +0900

    StageEditor 뼈대 제작
    화면 실행 및 종료 버튼 구현

--- Drills/2DGP_Project/2DGP_Project_StageEditor.py
+++ Drills/2DGP_Project/2DGP_Project_StageEditor.py
@@ -1,1 +1,28 @@
 from pico2d import *
+
+open_canvas()
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+    #for event in events:
+        #if event.type == SDL_MOUSEBUTTONDOWN:
+            # 마우스 위치 값, 현재 선택한 타일 정보 필요
+
+def draw_scene():
+    clear_canvas()
+    update_canvas()
+    handle_events()
+
+running = True
+
+while running:
+    draw_scene()
+
+close_canvas()
+
