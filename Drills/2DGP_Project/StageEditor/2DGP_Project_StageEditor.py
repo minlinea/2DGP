22af21876d1e2a8ce171369e59983c7a78cb6569
JungMinSu(MSI) <min_linea@naver.com>
2018-10-01 22:00:35 +0900

    파일정리 및 스테이지 에디터 배경그리기
    폴더에 내용에 따른 파일 생성 및 구분
    스테이지 에디터에 배경만 그려 놓음

--- Drills/2DGP_Project/StageEditor/2DGP_Project_StageEditor.py
+++ Drills/2DGP_Project/StageEditor/2DGP_Project_StageEditor.py
@@ -16,10 +16,16 @@ def handle_events():
 
 def draw_scene():
     clear_canvas()
+    whiteboard.clip_draw(0,0,800-1,600-1,(800-1)/2, (600-1)/2)
+    imposible_collocate.clip_draw(0, 0, 150 - 1, 400 - 1, (150 - 1) / 2, (400-1)/2)
+    imposible_collocate.clip_draw(0, 0, 150 - 1, 400 - 1, 800 - ((150 - 1) / 2), ((400 - 1) / 2))
     update_canvas()
     handle_events()
 
 running = True
+whiteboard = load_image('whiteboard.png')
+imposible_collocate = load_image('imposible_collocate.png')
+
 
 while running:
     draw_scene()
