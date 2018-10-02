8e469a5e28e05faa1676aabffe5f6d00b9e14df8
JungMinSu(MSI) <min_linea@naver.com>
2018-10-02 19:08:27 +0900

    movement_calculation 내용 수정
    momentum_control (이동량 조절 변수)이용 이동량 조절

--- Drills/Drill-07/Drill-07.py
+++ Drills/Drill-07/Drill-07.py
@@ -21,12 +21,13 @@ facing_point = 0
 
 image_size = 100
 image_point = 100
-grass_pibotx, grass_piboty = 400, 30
 
 frame = 0
 point = 1
 momentum_control = 10
+i = 1
 
+character_x, character_y = point_dictionary[0][0], point_dictionary[0][1]
 running = True
 
 def handle_events():
@@ -39,13 +40,13 @@ def handle_events():
             running = False
 
 def straight_move():
-    global point
-    character_x, character_y = point_dictionary[point-1]
-    destination_x, destination_y =  point_dictionary[point]
+    global point, character_x, character_y
     while (moving_direction(character_x, point_dictionary[point-1][0], point_dictionary[point][0])):
-        point = (point + 1) % momentum_control
-        draw_scene(character_x,character_y)
-        character_x, character_y = movement_calculation(character_x, character_y, destination_x, destination_y)
+        draw_scene(character_x, character_y)
+        character_x, character_y = movement_calculation(point_dictionary[point - 1][0], point_dictionary[point - 1][0],
+                                                        point_dictionary[point][0], point_dictionary[point][1])
+
+
     point = (point + 1) % momentum_control
     character_x, character_y = point_dictionary[point]
 
@@ -53,42 +54,37 @@ def straight_move():
 def draw_scene(character_X, character_Y):
     global frame
     clear_canvas()
-    character.clip_draw(frame * image_point, facing_direction[facing_point] * image_point, image_size, image_size, character_X, character_Y)
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+    character.clip_draw(frame * image_point, facing_direction[facing_point] * image_point, image_size, image_size, character_X, character_Y)
     update_canvas()
     frame = (frame + 1) % 8
     delay(0.05)
     get_events()
+    handle_events()
 
 
 def moving_direction(character_X, now_indexX, next_indexX):
     global facing_point
     if(next_indexX - now_indexX > 0):
         if (character_X + 1 < next_indexX):
-            if(next_indexX - character_X > 6):
-                facing_point  = run_right
-            else :
-                facing_point = walk_right
+            facing_point  = run_right
             return True
         else:
             return False
     else:
         if (character_X > next_indexX + 1):
-            if(character_X - next_indexX > 4):
-                facing_point  = run_left
-            else :
-                facing_point = walk_left
+            facing_point  = run_left
             return True
         else:
             return False
 
 
 def movement_calculation(x1, y1, x2, y2):
-    global momentum_control
-    for i in range(0, 100 + 1, 10):
-        t = i / 100
-        x = (1 - t) * x1 + t * x2
-        y = (1 - t) * y1 + t * y2
+    global momentum_control, i
+    i = ((i+1) % momentum_control)
+    t = i / momentum_control
+    x = (1 - t) * x1 + t * x2
+    y = (1 - t) * y1 + t * y2
     return (x, y)
 
 
