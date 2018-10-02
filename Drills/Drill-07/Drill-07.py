3bdb57fe42e0f7221685e9009b80fbf913804d33
JungMinSu(MSI) <min_linea@naver.com>
2018-10-02 18:44:09 +0900

    Drill-07 시작
    일단 코드를 그대로 가져오면서 일단 오늘 수업한 내용을 같이 버무렸음
    1. 리스트 빠르게 만들기
    2. 벡터 이동

--- Drills/Drill-07/Drill-07.py
+++ Drills/Drill-07/Drill-07.py
@@ -1,67 +1,93 @@
-import turtle
 import random
+from pico2d import *
+
+KPU_WIDTH, KPU_HEIGHT = 1280, 1024
+
+open_canvas()
+
+kpu_ground = load_image('KPU_GROUND.png')
+character = load_image('animation_sheet.png')
+
+size = 20
+
+point_dictionary = [(random.randint(0,800), random.randint(0,600)) for i in range(size)]
+
+run_left = 0
+run_right = 1
+walk_left = 2
+walk_right = 3
+facing_direction = (run_left, run_right, walk_left, walk_right)
+facing_point = 0
+
+image_size = 100
+image_point = 100
+grass_pibotx, grass_piboty = 400, 30
+
+now_index = 1
+frame = 0
+point = 1
+momentum_control = 10
+
+def straight_move():
+    global point
+    character_x, character_y = point_dictionary[point-1]
+    destination_x, destination_y =  point_dictionary[point]
+    while (moving_direction(character_x, point_dictionary[point-1][0], point_dictionary[point][0])):
+        point = (point + 1) % momentum_control
+        draw_scene(character_x,character_y)
+        character_x, character_y = movement_calculation(character_x, character_y, destination_x, destination_y)
+    point = (point + 1) % momentum_control
+    character_x, character_y = point_dictionary[now_index]
 
 
-def stop():
-    turtle.bye()
+def draw_scene(character_X, character_Y):
+    global frame
+    clear_canvas()
+    character.clip_draw(frame * image_point, facing_direction[facing_point] * image_point, image_size, image_size, character_X, character_Y)
+    kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+    update_canvas()
+    frame = (frame + 1) % 8
+    delay(0.05)
+    get_events()
 
 
-def prepare_turtle_canvas():
-    turtle.setup(1024, 768)
-    turtle.bgcolor(0.2, 0.2, 0.2)
-    turtle.penup()
-    turtle.hideturtle()
-    turtle.shape('arrow')
-    turtle.shapesize(2)
-    turtle.pensize(5)
-    turtle.color(1, 0, 0)
-    turtle.speed(100)
-    turtle.goto(-500, 0)
-    turtle.pendown()
-    turtle.goto(480, 0)
-    turtle.stamp()
-    turtle.penup()
-    turtle.goto(0, -360)
-    turtle.pendown()
-    turtle.goto(0, 360)
-    turtle.setheading(90)
-    turtle.stamp()
-    turtle.penup()
-    turtle.home()
-
-    turtle.shape('circle')
-    turtle.pensize(1)
-    turtle.color(0, 0, 0)
-    turtle.speed(50)
-
-    turtle.onkey(stop, 'Escape')
-    turtle.listen()
+def moving_direction(character_X, now_indexX, next_indexX):
+    global facing_point
+    if(next_indexX - now_indexX > 0):
+        if (character_X + 1 < next_indexX):
+            if(next_indexX - character_X > 6):
+                facing_point  = run_right
+            else :
+                facing_point = walk_right
+            return True
+        else:
+            return False
+    else:
+        if (character_X > next_indexX + 1):
+            if(character_X - next_indexX > 4):
+                facing_point  = run_left
+            else :
+                facing_point = walk_left
+            return True
+        else:
+            return False
 
 
-def draw_big_point(p):
-    turtle.goto(p)
-    turtle.color(0.8, 0.9, 0)
-    turtle.dot(15)
-    turtle.write('     '+str(p))
+def movement_calculation(x1, y1, x2, y2):
+    global momentum_control
+    for i in range(0, 100 + 1, 10):
+        t = i / 100
+        x = (1 - t) * x1 + t * x2
+        y = (1 - t) * y1 + t * y2
+    return (x, y)
 
 
-def draw_point(p):
-    turtle.goto(p)
-    turtle.dot(5, random.random(), random.random(), random.random())
+def move_to_point():
+    straight_move()
 
 
-def draw_line_basic(p1, p2):
-    # fill here
-    pass
+while True:
+    move_to_point()
 
 
-def draw_line(p1, p2):
-    # fill here
-   pass
-
-
-prepare_turtle_canvas()
-
-# fill here
-
-turtle.done()
+close_canvas()
