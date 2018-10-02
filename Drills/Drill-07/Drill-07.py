08fab3b55e1697f867d7d9b2ad2628045d9a0c45
JungMinSu(MSI) <min_linea@naver.com>
2018-10-02 22:40:32 +0900

    handle_events() 수행 어려움에 따른 새 함수 생성
    handle_events() 명령이 잘 이루어지지 않아 종료 이벤트가 잘 수행되지 않아 drawscene()을 메인에서 부르고 방향지정 함수를 while문에서 제거 하고, 목표점 도달시 변경하는 것 함수형태 제작

--- Drills/Drill-07/Drill-07.py
+++ Drills/Drill-07/Drill-07.py
@@ -9,9 +9,6 @@ kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 
 size = 20
-
-point_dictionary = [(random.randint(0,800), random.randint(0,600)) for i in range(size)]
-
 run_left = 0
 run_right = 1
 walk_left = 2
@@ -24,11 +21,14 @@ image_point = 100
 
 frame = 0
 point = 1
-momentum_control = 10
-i = 1
+momentum_control = 20
+cycle = 1
 
-character_x, character_y = point_dictionary[0][0], point_dictionary[0][1]
 running = True
+
+point_dictionary = [(random.randint(0 + image_size, 800 - image_size), random.randint(0 + image_size, 600 - image_size)) for i in range(size)]
+
+character_x, character_y = point_dictionary[0]
 
 def handle_events():
     global running
@@ -40,55 +40,49 @@ def handle_events():
             running = False
 
 def straight_move():
-    global point, character_x, character_y
-    while (moving_direction(character_x, point_dictionary[point-1][0], point_dictionary[point][0])):
-        draw_scene(character_x, character_y)
-        character_x, character_y = movement_calculation(point_dictionary[point - 1][0], point_dictionary[point - 1][1],
+    global point, character_x, character_y, running
+    moving_direction(character_x, point_dictionary[point-1][0], point_dictionary[point][0])
+    character_x, character_y = movement_calculation(point_dictionary[point - 1][0], point_dictionary[point - 1][1],
                                                         point_dictionary[point][0], point_dictionary[point][1])
-
     character_x, character_y = point_dictionary[point]
-    point = (point + 1) % momentum_control
+    point = (point) % momentum_control + 1
 
+def Reach_destination():
+    pass
 
-def draw_scene(character_X, character_Y):
+def draw_scene():
     global frame
+    handle_events()
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    character.clip_draw(frame * image_point, facing_direction[facing_point] * image_point, image_size, image_size, character_X, character_Y)
+    character.clip_draw(frame * image_point, facing_direction[facing_point] * image_point, image_size, image_size, character_x, character_y)
     update_canvas()
     frame = (frame + 1) % 8
     delay(0.05)
     get_events()
-    handle_events()
+
 
 
 def moving_direction(character_X, now_indexX, next_indexX):
     global facing_point
     if(next_indexX - now_indexX > 0):
-        if (character_X + 1 < next_indexX):
-            facing_point  = run_right
-            return True
-        else:
-            return False
+        facing_point = run_right
     else:
-        if (character_X > next_indexX + 1):
-            facing_point  = run_left
-            return True
-        else:
-            return False
+        facing_point = run_left
+
 
 
 def movement_calculation(x1, y1, x2, y2):
-    global momentum_control, i
-    i = ((i+1) % momentum_control)
-    t = i / momentum_control
+    global momentum_control, cycle
+    cycle = ((cycle+1) % momentum_control)
+    t= cycle / momentum_control
     x = (1 - t) * x1 + t * x2
     y = (1 - t) * y1 + t * y2
-    if(i == momentum_control-1):
+    if(cycle == momentum_control-1):
         t = 1
         x = (1 - t) * x1 + t * x2
         y = (1 - t) * y1 + t * y2
-        i = 0
+        cycle = 1
         t = 0
     return (x, y)
 
@@ -99,6 +93,7 @@ def move_to_point():
 
 while running:
     move_to_point()
+    draw_scene()
 
 
 close_canvas()
