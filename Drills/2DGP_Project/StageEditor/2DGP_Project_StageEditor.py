09108f676883115bc3241245dc1101ab4925800c
JungMinSu(MSI) <min_linea@naver.com>
2018-10-02 23:40:46 +0900

    Revert "키입력 부분 추가"
    This reverts commit 2df631c93d417581391ea48dd67c35887e5c6b9c.

--- Drills/2DGP_Project/StageEditor/2DGP_Project_StageEditor.py
+++ Drills/2DGP_Project/StageEditor/2DGP_Project_StageEditor.py
@@ -4,8 +4,6 @@ open_canvas()
 
 WINDOW_WIDTH, WINDOW_HEIGHT = 800, 600
 
-mouse_xpos, mouse_ypos = 0,0
-
 running = True
 click = False
 whiteboard = load_image('whiteboard.png')
@@ -16,7 +14,7 @@ ex_tile_direction = load_image('ex_tile_direction.png')
 def collocate_tile():
     pass
 
-def set_collocate_tile(num):
+def set_collocate_tile():
     pass
 
 def clear_stage():
@@ -29,42 +27,23 @@ def load_stage():
     pass
 
 def handle_events():
-    global running, click, mouse_xpos, mouse_ypos
+    global running, click
     events = get_events()
     for event in events:
-        if event.type == SDL_QUIT:                  # 마우스쪽
+        if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
         elif event.type == SDL_MOUSEBUTTONDOWN:
-            mouse_xpos, mouse_ypos = event.x, window_to_pico_coordinate_system(event.y)
             click = True # 선택한 상태에서 마우스모션과 연계
-            collocate_tile()    #elif 어떤 범위 안이면
             # 마우스 위치 값, 현재 선택한 타일 정보 필요
             pass
         elif event.type == SDL_MOUSEMOTION and click == True:
-            mouse_xpos, mouse_ypos = event.x, window_to_pico_coordinate_system(event.y)
             # 마우스를 누른채로 움직이면 한번에 타일이 쫘르륵 그려지게끔 만들자.
             pass
         elif event.type == SDL_MOUSEBUTTONUP:
             click = False # 마우스 버튼 뗀 순간 마우스모션과 연계 안되게끔
             pass
-        elif event.type == SDL_KEYDOWN:         #키 입력 부분
-            if event.key == SDLK_1:
-                set_collocate_tile(1)
-                pass        #1번타일 로드
-            elif event.key == SDLK_2:
-                set_collocate_tile(2)
-                pass        #2번타일 로드.. 아마 8번 타일까지 만드려나?
-            elif event.key == SDLK_9:
-                save_stage()
-                pass        # 그렸던 것 저장
-            elif event.key == SDLK_0:
-                load_stage()
-                pass        # 그렸던 것 로드
-            elif event.key == SDLK_r:
-                clear_stage()
-                pass        #맵 초기화
 
 def window_to_pico_coordinate_system(num):
     return WINDOW_HEIGHT - 1 - num
