d98fff11554cb0d45369595ebc82b514a80b65b1
JungMinSu(MSI) <min_linea@naver.com>
2018-10-04 18:28:44 +0900

    p4 - p1 반복부분 추가
    반복함에 따라 p4-p1의 전반부 (0~50 절반까지) 곡선 이동하는 cardinal_spline 공식 추가 및 점 수정
    했더니 바로 됐어? 와,,

--- Drills/Drill-08/Drill-08-1.py
+++ Drills/Drill-08/Drill-08-1.py
@@ -98,7 +98,12 @@ def draw_curve_4_points(p1, p2, p3, p4):
     draw_point(p3)
 
     # draw p4-p1
-    
+    for i in range(0, 50, 2):
+        t = i / 100
+        x = (2*t**2-3*t+1)*p4[0]+(-4*t**2+4*t)*p1[0]+(2*t**2-t)*p2[0]
+        y = (2*t**2-3*t+1)*p4[1]+(-4*t**2+4*t)*p1[1]+(2*t**2-t)*p2[1]
+        draw_point((x, y))
+    draw_point(p4)
 
 
 
