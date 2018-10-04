26a50b3bfe3bc7f27ed3e3d14db999bfbbc856ed
JungMinSu(MSI) <min_linea@naver.com>
2018-10-04 18:30:53 +0900

    Drill-08-1에서 작성한 실습내용 가져옴
    똑같이 이동에 필요하다고 생각된 해당코드를 가져왔으며 필요없는 draw_point와 함수의 가독성을 위한 이름 수정

--- Drills/Drill-08/Drill-08-2.py
+++ Drills/Drill-08/Drill-08-2.py
@@ -49,6 +49,35 @@ def straight_move():
         character_x, character_y = point_dictionary[point]
         point = (point+1) % size
 
+def cardinal_spline_move(p1, p2, p3, p4):
+
+    # draw p1-p2
+    for i in range(0, 50, 2):
+        t = i / 100
+        x = (2*t**2-3*t+1)*p1[0]+(-4*t**2+4*t)*p2[0]+(2*t**2-t)*p3[0]
+        y = (2*t**2-3*t+1)*p1[1]+(-4*t**2+4*t)*p2[1]+(2*t**2-t)*p3[1]
+
+
+    # draw p2-p3
+    for i in range(0, 100, 2):
+        t = i / 100
+        x = ((-t**3 + 2*t**2 - t)*p1[0] + (3*t**3 - 5*t**2 + 2)*p2[0] + (-3*t**3 + 4*t**2 + t)*p3[0] + (t**3 - t**2)*p4[0])/2
+        y = ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
+
+    # draw p3-p4
+    for i in range(0, 100, 2):
+        t = i / 100
+        x = ((-t**3 + 2*t**2 - t)*p2[0] + (3*t**3 - 5*t**2 + 2)*p3[0] + (-3*t**3 + 4*t**2 + t)*p4[0] + (t**3 - t**2)*p1[0])/2
+        y = ((-t**3 + 2*t**2 - t)*p2[1] + (3*t**3 - 5*t**2 + 2)*p3[1] + (-3*t**3 + 4*t**2 + t)*p4[1] + (t**3 - t**2)*p1[1])/2
+
+
+    # draw p4-p1
+    for i in range(0, 50, 2):
+        t = i / 100
+        x = (2*t**2-3*t+1)*p4[0]+(-4*t**2+4*t)*p1[0]+(2*t**2-t)*p2[0]
+        y = (2*t**2-3*t+1)*p4[1]+(-4*t**2+4*t)*p1[1]+(2*t**2-t)*p2[1]
+
+
 def Reach_destination():
     pass
 
