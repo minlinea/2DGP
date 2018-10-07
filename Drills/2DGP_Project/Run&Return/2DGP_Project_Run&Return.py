9eeac77e78efd55f76bf52b95739a9e37530af15
JungMinSu(MSI) <min_linea@naver.com>
2018-10-07 21:10:06 +0900

    캐릭터 움직임 구현
    Drill-04에 있는 run_animation 이미지 참고하여 달리는 모습 및 프레임구현

--- Drills/2DGP_Project/Run&Return/2DGP_Project_Run&Return.py
+++ Drills/2DGP_Project/Run&Return/2DGP_Project_Run&Return.py
@@ -8,8 +8,13 @@ tile_size = 40
 tile_information_kind = [([(0) for i in range(20)]) for j in range(15)]
 
 tile_kind = load_image('tile_kind.png')
+character = load_image('run_animation.png')
 running = True
 
+move = False
+frame = 0
+character_xpos = 800//2
+dir = 0
 
 
 def load_stage():           # 'save_stage'에 저장되어 있는 타일 파일 로드하여 정보 저장
@@ -22,7 +27,7 @@ def load_stage():           # 'save_stage'에 저장되어 있는 타일 파일 
     file.close()
 
 def handle_events():
-    global running
+    global running, dir, move
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -30,19 +35,35 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 #------------------------------------------- 마우스 처리----------------------------------------------------#
-        elif event.type == SDL_KEYDOWN:
-            if event.key == SDLK_1:      #왼쪽 1번째줄 타일 셋
-                load_stage()
+
 # ------------------------------------------- 마우스 처리----------------------------------------------------#
 
 # --------------------------------------- 키보드 입력 처리----------------------------------------------------#
-
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_1:      #왼쪽 1번째줄 타일 셋
+                load_stage()
+            if event.key == SDLK_RIGHT:
+                dir += 0.5
+                move = True
+            elif event.key == SDLK_LEFT:
+                dir -= 0.5
+                move = True
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir -= 0.5
+                move = False
+            elif event.key == SDLK_LEFT:
+                dir += 0.5
+                move = False
 # --------------------------------------- 키보드 입력 처리----------------------------------------------------#
 
 def window_to_pico_coordinate_system(num):      # pico 환경과, 윈도우 환경 마우스 좌표 값 조정 함수
     return WINDOW_HEIGHT - 1 - num
 
 def draw_scene():
+    global frame, character_xpos
     clear_canvas()
 
     for j in range(0, 15, 1):
@@ -50,6 +71,12 @@ def draw_scene():
             tile_kind.clip_draw(5 + (42 * ((tile_information_kind[j][i]) % 2)),4 + ((42*4)-(42 * ((tile_information_kind[j][i]+2)// 2))),
                                 tile_size, tile_size, 20 + i*tile_size, 20 + j * tile_size)
 
+    character.clip_draw(frame * 100, 0, 100, 100, character_xpos, 600//2)
+    if move == True:
+        frame = (frame + 1) % 8
+    else:
+        frame = 0
+    character_xpos += dir
     update_canvas()
     handle_events()
 
