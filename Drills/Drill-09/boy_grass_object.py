08574e230d7e772177fbaac26944274adf3f17dd
JungMinSu(MSI) <min_linea@naver.com>
2018-10-09 18:20:42 +0900

    클래스를 이용한 Lecture08 실습
    클래스의 정의, 생성, 내용에 대한 교육 및
    게임 기본 구조와 게임 루프에 대한 교육 후 그걸 기반으로 한 현재의 코드 내용 작성

--- Drills/Drill-09/boy_grass_object.py
+++ Drills/Drill-09/boy_grass_object.py
@@ -1,6 +1,26 @@
 from pico2d import *
 
 # Game object class here
+class Grass:
+    def __init__(self):     #생성자 함수 self = this, 속성을 정의해준다.
+        self.image = load_image('grass.png')    #속성의 생성과 함께 초기화도 해준다.
+
+    def draw(self):         #행위, 속성을 변화시키는 것
+        self.image.draw(400, 30)
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = 0, 90
+        self.frame = 0
+        self.image = load_image('run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 5
+
+    def draw(self):
+        self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
+
 
 def handle_events():
     global running
@@ -12,7 +32,27 @@ def handle_events():
             running = False
 
 # initialization code
+open_canvas()
+
+boy = Boy()
+grass = Grass()
+
+running = True
 
 # game main loop code
 
+while running:
+    handle_events()     #사용자 입력
+
+    boy.update()        #게임 로직
+
+    clear_canvas()      #렌더링
+    grass.draw()
+    boy.draw()
+    update_canvas()
+
+    delay(0.05)     # --> handle_events(사용자 입력)로 돌아가는 것 = 게임 루프
+
 # finalization code
+
+close_canvas()
