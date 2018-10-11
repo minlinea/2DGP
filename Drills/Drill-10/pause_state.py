7c0ab13b612e848cc5897fb6373a48fe64c9f9b1
JungMinSu(MSI) <min_linea@naver.com>
2018-10-11 18:48:35 +0900

    Drill-10-1 완성
    Push, Pop 기능 이용 Stack 자료 구조를 이용하여 pause_state 호출, 제거하면서 상태 유지하는 것 완성

--- Drills/Drill-10/pause_state.py
+++ Drills/Drill-10/pause_state.py
@@ -37,7 +37,7 @@ def handle_events():
         if event.type == SDL_QUIT:
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_p:
-            game_framework.change_state(main_state)
+            game_framework.pop_state()
 
 
 def pause(): pass
