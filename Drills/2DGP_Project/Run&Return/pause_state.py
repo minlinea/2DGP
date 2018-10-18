04d57feab7c5add9f2d1aaee10ef91bf32fb5297
JungMinSu(MSI) <min_linea@naver.com>
2018-10-19 00:04:56 +0900

    pause 항목 이미지 로드
    퍼즈 실행시 pause_choose_menu이미지 구현, 위치가 조금 안 맞는지 약간의 움직임 수정 필요

--- Drills/2DGP_Project/Run&Return/pause_state.py
+++ Drills/2DGP_Project/Run&Return/pause_state.py
@@ -2,7 +2,7 @@ import game_framework
 import stage_run
 from pico2d import *
 
-
+WINDOW_HEIGHT = 600
 pause_image = None
 choose_menu = None
 choose_menu_pivot_num = 0
@@ -27,21 +27,34 @@ def update():
 def draw():
     global pause_image, choose_menu
     clear_canvas()
-    #choose_menu.clip_draw(400,300)
     pause_image.draw(400,300)
+    choose_menu.clip_draw(613 * choose_menu_pivot_num,0,613,370,(706+91)/2, WINDOW_HEIGHT - (592+223)/2)
     update_canvas()
 
 
 
 
 def handle_events():
+    global choose_menu_pivot_num
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.pop_state()
-    pass
+        elif event.type == SDL_MOUSEMOTION:
+            if(event.x < 593 and event.x > 90):
+                if (window_to_pico_coordinate_system(event.y) < 355):
+                    choose_menu_pivot_num = 1
+                else:
+                    choose_menu_pivot_num = 0
+            pass
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            pass
+
+def window_to_pico_coordinate_system(num):      # pico 환경과, 윈도우 환경 마우스 좌표 값 조정 함수
+    return WINDOW_HEIGHT - 1 - num
+
 
 
 def pause(): pass
