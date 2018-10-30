1f2b1d41584804097c6f2cebf1736c69315aecb4
JungMinSu(MSI) <min_linea@naver.com>
2018-10-30 22:43:27 +0900

    wakeup 후 이미지 투명, 반투명도 랜덤 설정
    랜덤한 값을 받아 0이면 투명도 증가, 1이면 투명도 감소

--- Drills/Drill-12/ghost.py
+++ Drills/Drill-12/ghost.py
@@ -1,4 +1,6 @@
 import game_framework
+import math
+import random
 from pico2d import *
 
 
@@ -12,7 +14,10 @@ RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
-# Boy Action Speed
+TIME_PER_CIRCLE = 0.5
+CIRCLE_PER_TIME = 1.0 / TIME_PER_CIRCLE
+FRAMES_PER_DEGREE = 360
+
 # fill expressions correctly
 TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
@@ -26,7 +31,8 @@ class Ghost:
         if Ghost.image == None:
             self.image = load_image('animation_sheet.png')
         self.dir = dir
-        self.velocity = 0
+        self.x_velocity = 0.0
+        self.y_velocity = 0.0
         self.frame = 0
         self.degree = dir * 3.141592 / 2
         self.opacify = 0.01
@@ -37,16 +43,30 @@ class Ghost:
         if self.wakeup is False :
             self.opacify +=  game_framework.frame_time / 4.0
             self.degree = self.degree + game_framework.frame_time * -self.dir * 3.141592 / 2 / 3.9
+            #self.y_velocity += self.degree
+            #self.y += self.y_velocity
             if self.opacify >= 1:
+                self.opacify = 1
                 self.wakeup = True
         else:
-            pass
+            if random.randint(0,1) == 0 or self.opacify == 1:
+                self.opacify -= 0.01;
+            elif random.randint(0,1) == 1 or self.opacify == 0:
+                self.opacify += 0.01;
+
 
     def draw(self):
         self.image.opacify(self.opacify)
-        if self.dir == 1:
-            self.image.clip_composite_draw(int(self.frame) * 100, 300, 100, 100, self.degree, '', self.x - 25, self.y - 25 + 100,
+        if(self.wakeup is False):
+            if self.dir == 1:
+                self.image.clip_composite_draw(int(self.frame) * 100, 300, 100, 100, self.degree, '', self.x - 25, self.y - 25,
                                           100, 100)
+            else:
+                self.image.clip_composite_draw(int(self.frame) * 100, 200, 100, 100, self.degree, '', self.x + 25,
+                                           self.y - 25, 100, 100)
         else:
-            self.image.clip_composite_draw(int(self.frame) * 100, 200, 100, 100, self.degree, '', self.x + 25,
-                                           self.y - 25 + 100, 100, 100)
+            pass
+            if self.dir == 1:
+                self.image.clip_draw(int(self.frame) * 100, 300, 100, 100, self.x, self.y)
+            else:
+                self.image.clip_draw(int(self.frame) * 100, 200, 100, 100, self.x, self.y)
