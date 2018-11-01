c3aacbf28b6858d667d4036d9a2bbd09f07ee2c0
JungMinSu(MSI) <min_linea@naver.com>
2018-11-01 20:43:21 +0900

    게임 프레임워크 시간 추가
    Drill12 활용 필요내용 추가

--- Drills/2DGP_Project/Run&Return/game_framework.py
+++ Drills/2DGP_Project/Run&Return/game_framework.py
@@ -1,3 +1,5 @@
+import time
+
 class GameState:
     def __init__(self, state):
         self.enter = state.enter
@@ -81,17 +83,25 @@ def quit():
     global running
     running = False
 
+frame_time = 0.0
 
 def run(start_state):
     global running, stack
     running = True
     stack = [start_state]
     start_state.enter()
+
+    global frame_time
+    current_time = time.time()
     while (running):
         stack[-1].handle_events()
         stack[-1].update()
         stack[-1].draw()
-    # repeatedly delete the top of the stack
+        # fill here
+        frame_time = time.time() - current_time
+        frame_rate = 1.0 / frame_time
+        current_time += frame_time
+
     while (len(stack) > 0):
         stack[-1].exit()
         stack.pop()
