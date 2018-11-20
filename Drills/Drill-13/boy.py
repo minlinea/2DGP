5b2e0befb71f1765136acc091929dea1c3db1c64
JungMinSu(MSI) <min_linea@naver.com>
2018-11-20 18:47:18 +0900

    위 / 아래 펜스 clamp 설정
    구한 좌표를 바탕으로 위쪽 아래쪽 펜스에 대한 clamp 좌표 설정

--- Drills/Drill-13/boy.py
+++ Drills/Drill-13/boy.py
@@ -71,7 +71,7 @@ class WalkingState:
         boy.y += boy.y_velocity * game_framework.frame_time
 
         boy.x = clamp(0, boy.x, boy.bg.w)
-        boy.y = clamp(0, boy.y, boy.bg.h)
+        boy.y = clamp(77, boy.y, 1087)
 
 
     @staticmethod
