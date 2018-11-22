756e3641fb5f160ee1ac367cdf4bd472088387d2
JungMinSu(MSI) <min_linea@naver.com>
2018-11-22 18:35:23 +0900

    윈도우 이미지 변경 및 공 그리기
    Drill에 맞는 윈도우 이미지 축소, 공 그려내기만 함

--- Drills/Drill-14/main_state.py
+++ Drills/Drill-14/main_state.py
@@ -9,12 +9,27 @@ import game_world
 from boy import Boy
 from background import FixedBackground as Background
 #from background import InfiniteBackground as Background
-
+from ball import Ball
 
 name = "MainState"
 
 boy = None
 background = None
+
+balls = []
+
+
+def collide(a, b):
+    # fill here
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+
+    if left_a > right_b: return False
+    if right_a < left_b: return False
+    if top_a < bottom_b: return False
+    if bottom_a > top_b: return False
+
+    return True
 
 
 def enter():
@@ -26,7 +41,10 @@ def enter():
     background = Background()
     game_world.add_object(background, 0)
 
-    # fill here
+    global balls
+    balls = [Ball() for i in range(10)]
+    game_world.add_objects(balls, 1)
+
     background.set_center_object(boy)#상호참조
     boy.set_background(background)
 
@@ -57,7 +75,12 @@ def handle_events():
 def update():
     for game_object in game_world.all_objects():
         game_object.update()
-
+    for ball in balls:
+        if collide(boy, ball):
+            balls.remove(ball)
+            # fill here
+            boy.eat(ball)
+            game_world.remove_object(ball)
 
 def draw():
     clear_canvas()
